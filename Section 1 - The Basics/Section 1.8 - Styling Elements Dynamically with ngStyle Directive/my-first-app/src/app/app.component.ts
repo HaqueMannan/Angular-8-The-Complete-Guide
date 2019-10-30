5c06b8ae6c2ed4d90158111577745ec5e97cb146
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-10-30 22:16:01 +0000

    Section 1 - The Basics - 1.8. Styling Elements Dynamically with ngStyle Directive.

---
+++ Section 1 - The Basics/Section 1.8 - Styling Elements Dynamically with ngStyle Directive/my-first-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+   // styles: [`
+   //    h3 {
+   //       color: dodgerblue;
+   //    }
+   // `]
+})
+export class AppComponent {
+  title = 'my-first-app';
+}
