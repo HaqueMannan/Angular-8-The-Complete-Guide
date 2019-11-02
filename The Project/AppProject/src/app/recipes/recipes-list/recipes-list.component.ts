40606287a790a57d3befe22fbcb7d1674a5b1f1d
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-11-02 02:58:35 +0000

    App Project - The Basics - 1.3. Recipe Model, Adding Content to the Recipes Components, Outputting a List of Recipes with ngFor and Displaying Recipe Details.

--- The Project/AppProject/src/app/recipes/recipes-list/recipes-list.component.ts
+++ The Project/AppProject/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
    selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+   recipes: Recipe[] = [
+      new Recipe('Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+      new Recipe('Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+   ];
 
    constructor() { }
 
