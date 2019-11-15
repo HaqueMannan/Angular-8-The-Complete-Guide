d8ee6ace6649806bfdb86fd4e7aa3996c13bbe4c
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-11-15 23:00:33 +0000

    App Project - Services & Dependency Injection - 5.2. Using a Service for Cross-Component Communication.

--- The Project/AppProject/src/app/recipes/recipe.service.ts
+++ The Project/AppProject/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+   recipeSelected = new EventEmitter<Recipe>();
+
    private recipes: Recipe[] = [
       new Recipe('Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
       new Recipe('Another Test Recipe', 'This is another test recipe', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
