5e57873580ac43ec9aa48012ff73f1e4dbdabac9
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-11-24 19:43:26 +0000

    App Project - Changing Pages with Routing - 6.2. Fixing Page Reload Issues and Adding Child Routes.

--- The Project/AppProject/src/app/recipes/recipe.service.ts
+++ The Project/AppProject/src/app/recipes/recipe.service.ts
@@ -9,7 +9,7 @@ export class RecipeService {
 
    private recipes: Recipe[] = [
       new Recipe(
-         'Steak and Salad',
+         'Steak and salad',
          'A big juicy steak with a side of health green salad.',
          'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
          [
@@ -20,7 +20,7 @@ export class RecipeService {
          ]
       ),
       new Recipe(
-         'Lamb Chops on a mediterranean salad',
+         'Lamb chops on a mediterranean salad',
          'Another super recipe that is a must try.',
          'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
          [
