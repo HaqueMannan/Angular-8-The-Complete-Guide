9515fe96dbfbc051d0d0b3da8470f28ce7bf0408
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-01 22:19:34 +0000

    App Project - Handling Forms in Angular Apps - 8.2. Allowing the Selection of Items in the List and Loading the Shopping List Items into the Form.

--- The Project/AppProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ The Project/AppProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,24 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { NgForm } from '@angular/forms';
 
 @Component({
    selector: 'app-shopping-edit',
    templateUrl: './shopping-edit.component.html',
    styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+   @ViewChild('form', { static: false }) shoppingListForm: NgForm;
+   subscription: Subscription;
+   editMode = false;
+   editedItemIndex: number;
+   editedItem: Ingredient;
 
    constructor(private shoppingListService: ShoppingListService) { }
 
    ngOnInit() {
+      this.subscription = this.shoppingListService.startedEditing.subscribe(
+         (index: number) => {
+            this.editedItemIndex = index;
+            this.editMode = true;
+            this.editedItem = this.shoppingListService.getIngredient(index);
+            this.shoppingListForm.setValue({
+               name: this.editedItem.name,
+               amount: this.editedItem.amount
+            })
+         }
+      );
    }
 
    onAddItem(form: NgForm) {
       const value = form.value;
       const newIngredient = new Ingredient(value.name, value.amount);
       this.shoppingListService.addIngredient(newIngredient);
+   }
+
+   ngOnDestroy() {
+      this.subscription.unsubscribe();
    }
 }
