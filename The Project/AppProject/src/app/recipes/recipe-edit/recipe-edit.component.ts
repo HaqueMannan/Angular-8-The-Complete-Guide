7db59f69940b799e84c87930d9b27a8858002615
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-02 15:46:54 +0000

    App Project - Handling Forms in Angular Apps - 8.8. Adding a Delete and Clear (Cancel) Functionality and Redirecting the User (After Deleting a Recipe).

--- The Project/AppProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ The Project/AppProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
+// import { Recipe } from '../recipe.model';
 
 @Component({
    selector: 'app-recipe-edit',
@@ -15,7 +15,7 @@ export class RecipeEditComponent implements OnInit {
    editMode = false;
    recipeForm: FormGroup;
 
-   constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
+   constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) {}
 
    ngOnInit() {
       this.route.params.subscribe(
@@ -41,6 +41,7 @@ export class RecipeEditComponent implements OnInit {
       } else {
          this.recipeService.addRecipe(this.recipeForm.value);
       }
+      this.onCancel();
    }
 
    get controls() {// a getter!
@@ -54,6 +55,10 @@ export class RecipeEditComponent implements OnInit {
             'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)]),
          })
       )
+   }
+
+   onCancel() {
+      this.router.navigate(['../'], { relativeTo: this.route });
    }
 
    private initForm() {
