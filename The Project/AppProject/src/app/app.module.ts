80948cffff79cee383093d3082c1d6b5b5a7bed9
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-02 16:06:29 +0000

    App Project - Handling Forms in Angular Apps - 8.9. Adding Image Preview, Providing the Recipe Service Correctly, Deleting Ingredients and Finishing Touches.

--- The Project/AppProject/src/app/app.module.ts
+++ The Project/AppProject/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -37,7 +38,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
       ReactiveFormsModule,
       AppRoutingModule
    ],
-   providers: [ShoppingListService],
+   providers: [ShoppingListService, RecipeService],
    bootstrap: [AppComponent]
 })
 export class AppModule { }
