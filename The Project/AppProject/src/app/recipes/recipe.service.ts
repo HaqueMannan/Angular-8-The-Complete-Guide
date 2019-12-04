6af383c686f2511b6d197b9c9539faf2a979219c
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-04 23:45:44 +0000

    App Project - Making Http Requests - 9.3. Transforming Response Data.

--- The Project/AppProject/src/app/recipes/recipe.service.ts
+++ The Project/AppProject/src/app/recipes/recipe.service.ts
@@ -9,30 +9,32 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
    recipeChanged = new Subject<Recipe[]>();
 
-   private recipes: Recipe[] = [
-      new Recipe(
-         'Steak and salad',
-         'A big juicy steak with a side of health green salad.',
-         'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-         [
-            new Ingredient('Sirloin Steak', 1),
-            new Ingredient('Lettuce', 1),
-            new Ingredient('Tomato', 1),
-            new Ingredient('Yellow Peppers', 1)
-         ]
-      ),
-      new Recipe(
-         'Lamb chops & mediterranean salad',
-         'Another super recipe that is a must try.',
-         'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
-         [
-            new Ingredient('Lamb Chops', 5),
-            new Ingredient('Red Chilli', 2),
-            new Ingredient('Red Onions', 1),
-            new Ingredient('Salad Dressing', 1),
-         ]
-      )
-   ];
+   // private recipes: Recipe[] = [
+   //    new Recipe(
+   //       'Steak and salad',
+   //       'A big juicy steak with a side of health green salad.',
+   //       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+   //       [
+   //          new Ingredient('Sirloin Steak', 1),
+   //          new Ingredient('Lettuce', 1),
+   //          new Ingredient('Tomato', 1),
+   //          new Ingredient('Yellow Peppers', 1)
+   //       ]
+   //    ),
+   //    new Recipe(
+   //       'Lamb chops & mediterranean salad',
+   //       'Another super recipe that is a must try.',
+   //       'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+   //       [
+   //          new Ingredient('Lamb Chops', 5),
+   //          new Ingredient('Red Chilli', 2),
+   //          new Ingredient('Red Onions', 1),
+   //          new Ingredient('Salad Dressing', 1),
+   //       ]
+   //    )
+   // ];
+
+   private recipes: Recipe[] = [];
 
    constructor(private shoppingListService: ShoppingListService) {}
 
