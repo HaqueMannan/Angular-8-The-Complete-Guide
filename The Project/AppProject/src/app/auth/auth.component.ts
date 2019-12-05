f2e4e5ecaeaed8ddeedeb970f9eb0857325d5194
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-05 19:32:29 +0000

    Section 11 - Authentication & Route Protection in Angular - 11.2. Switching between Auth Modes, Handling For Inout and Preparing the Backend.

--- The Project/AppProject/src/app/auth/auth.component.ts
+++ The Project/AppProject/src/app/auth/auth.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
    selector: 'app-auth',
    templateUrl: './auth.component.html'
 })
 
-export class AuthComponent implements OnInit {
-   constructor() { }
+export class AuthComponent {
+   isLoginMode = true;
 
-   ngOnInit() { }
+   onSwitchMode() {
+      this.isLoginMode = !this.isLoginMode
+   }
+
+   onSubmit(form: NgForm) {
+      console.log(form);
+      form.reset();
+   }
 }
