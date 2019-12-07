2df21ebbc3579d5036f0c8d81d9600509a738dbb
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-07 23:48:58 +0000

    Section 13 - Angular Modules & Optimising Angular Apps - 13.6. Modules & Services and Loading Services Differently.

--- The Project/AppProject/src/app/core.module.ts
+++ The Project/AppProject/src/app/core.module.ts
@@ -4,9 +4,11 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+// import { LoggingService } from './logging.service';
 
 @NgModule({
    providers: [
+      // LoggingService,
       ShoppingListService,
       RecipeService,
       { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
