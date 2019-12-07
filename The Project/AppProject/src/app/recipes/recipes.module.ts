6a90700a1cd0403c244fd04e754379184a0156aa
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-12-07 18:17:16 +0000

    Section 13 - Angular Modules & Optimising Angular Apps - 13.1. What are Modules, Analysing the AppModule, Getting Started with Feature Modules and Splitting Modules Correctly.

---
+++ The Project/AppProject/src/app/recipes/recipes.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes-detail/recipes-detail.component';
+import { RecipesItemComponent } from './recipes-list/recipes-item/recipes-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+@NgModule({
+   declarations: [
+      RecipesComponent,
+      RecipesListComponent,
+      RecipesDetailComponent,
+      RecipesItemComponent,
+      RecipeStartComponent,
+      RecipeEditComponent
+   ],
+   imports: [
+      RouterModule,
+      CommonModule,
+      ReactiveFormsModule
+   ],
+   exports: [
+      RecipesComponent,
+      RecipesListComponent,
+      RecipesDetailComponent,
+      RecipesItemComponent,
+      RecipeStartComponent,
+      RecipeEditComponent
+   ]
+})
+export class RecipesModule {
+
+}
