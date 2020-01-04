a94871720ec894bc0c4323cf48aadb9917c6aabf
Haque <34745319+HaqueMannan@users.noreply.github.com>
2020-01-04 15:53:41 +0000

    Section 17 - Angular Animations - 17.4. Advanced Transitions and Transition Phases.

---
+++ Section 17 - Angular Animations/17.4 - Advanced Transitions and Transition Phases/animations/src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   animations: [
+      trigger('divState', [
+         state('normal', style({
+            'background-color': 'red',
+            transform: 'translateX(0)'
+         })),
+         state('highlight', style({
+            'background-color': 'blue',
+            transform: 'translateX(100px)'
+         })),
+         transition('normal <=> highlight', animate(300)),
+         // transition('highlight => normal', animate(800))
+      ]),
+      trigger('wildState', [
+         state('normal', style({
+            'background-color': 'red',
+            transform: 'translateX(0) scale(1)'
+         })),
+         state('highlight', style({
+            'background-color': 'blue',
+            transform: 'translateX(100px) scale(1)'
+         })),
+         state('shrunk', style({
+            'background-color': 'green',
+            transform: 'translateX(0px) scale(0.5)'
+         })),
+         transition('normal <=> highlight', animate(300)),
+         transition('highlight => normal', animate(800)),
+         // transition('shrunk <=> *', animate(500, style({
+         //    borderRadius: '50px'
+         // })))
+         transition('shrunk <=> *', [
+            style({
+               'background-color': 'orange',
+               borderRadius: '0'
+            }),
+            animate(1000, style({
+               borderRadius: '50px'
+            })),
+            animate(500)
+         ])
+      ])
+   ]
+})
+export class AppComponent {
+   state = 'normal';
+   wildState = 'normal';
+   list = ['Milk', 'Sugar', 'Bread'];
+
+   onAnimate() {
+      this.state == 'normal' ? this.state = 'highlight' : this.state = 'normal';
+      this.wildState == 'normal' ? this.wildState = 'highlight' : this.wildState = 'normal';
+   }
+
+   onShrink() {
+      this.wildState = 'shrunk';
+   }
+
+   onAdd(item) {
+      this.list.push(item);
+   }
+}
